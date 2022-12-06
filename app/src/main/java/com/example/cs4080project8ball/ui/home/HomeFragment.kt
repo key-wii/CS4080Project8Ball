1f4fc03bb03912beebd574c8da48ef12c969a323
fquach <fifiq28@gmail.com>
2022-12-05 16:44:11 -0800

    argued with onCreateView function for a while until running configuration

--- app/src/main/java/com/example/cs4080project8ball/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cs4080project8ball/ui/home/HomeFragment.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.cs4080project8ball.R
 import com.example.cs4080project8ball.databinding.FragmentHomeBinding
 import kotlinx.android.synthetic.main.fragment_home.*
+
 
 class HomeFragment : Fragment() {
 
@@ -30,16 +31,19 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this)[HomeViewModel::class.java]
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        // inflates layout for fragment
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-        // put code here
+        view.apply {
 
-        return root
+            // put code here?
+
+            return view
+        }
     }
 
     override fun onDestroyView() {
